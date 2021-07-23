c9d3999ef01f901141dcb23c620d6bfc48d981f8
KozakovAlex <72526330+KozakovAlex@users.noreply.github.com>
2021-07-23 22:55:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,30 +5,86 @@ const clearScreen = document.querySelector('.clear-screen');
 const submit = document.querySelector('.submit');
 const backspace = document.querySelector('.backspace');
 
-// let outputValue = outputScreen.textContent;
-// console.log(outputValue);
+let outputValue = '';
 
 for(let numberButton of numberButtons) {
   numberButton.addEventListener('click', ()=> {
-    outputScreen.textContent += numberButton.getAttribute('data-value');
+    outputValue += numberButton.getAttribute('data-value');
+    outputScreen.textContent = outputValue;
+    // outputScreen.textContent += numberButton.getAttribute('data-value');
+    // outputValue = outputScreen.textContent;
+    // console.log(outputValue);
   })
 }
 
 for(let funcButton of funcButtons) {
   funcButton.addEventListener('click', ()=> {
-    outputScreen.textContent += funcButton.getAttribute('data-value');
+    outputValue += funcButton.getAttribute('data-value');
+    outputScreen.textContent = outputValue;
+    // console.log(outputValue);
   })
 }
 
 clearScreen.addEventListener('click', ()=> {
-  outputScreen.textContent = '';
+  outputValue = '';
+  outputScreen.textContent = outputValue;
 })
 
 backspace.addEventListener('click', () => {
-  outputScreen.textContent = outputScreen.textContent.slice(0, outputScreen.textContent.length - 1);
+  outputValue = outputValue.slice(0, outputValue.length - 1);
+  outputScreen.textContent = outputValue;
 })
 
 submit.addEventListener('click', ()=> {
+  let arrNumbers = [];
+  let arrSigns = [];
+  let currentNumber = '';
+  for(let i = 0; i < outputValue.length; i++) {
+    if(!isNaN(outputValue[i])) {
+      currentNumber += outputValue[i];
+    } else {
+      arrNumbers.push(currentNumber);
+      currentNumber = '';
+      arrSigns.push(outputValue[i]);
+    }
+  }
+  arrNumbers.push(currentNumber);
+  
+  const arrTrueNumbers = arrNumbers.map(item => Number(item));
+  console.log(arrTrueNumbers);
+  console.log(arrSigns);
 
+  outputValue = countResult(arrTrueNumbers, arrSigns);
+  outputScreen.textContent = outputValue;
+
+  // let result = 0;
+  // for(let i = 0; i < arrTrueNumbers.length; i++) {
+  //    if()
+  // }
+  // let plusIndex = outputValue.indexOf('+');
+  // console.log(plusIndex);
+  // let numOne = +outputValue.slice(0, plusIndex);
+  // console.log(numOne);
+  // let numTwo = +outputValue.slice(plusIndex + 1, outputValue.length);
+  // console.log(numTwo);
+  // outputValue = numOne + numTwo;
+  // outputScreen.textContent = outputValue;
 })
+
+function countResult(arr1, arr2) {
+  let result = 0;
+  if(arr2[0] == '+') {
+    result = arr1[0] + arr1[1];
+  }
+  if(arr2[0] == '-') {
+    result = arr1[0] - arr1[1];
+  }
+  if(arr2[0] == '*') {
+    result = arr1[0] * arr1[1];
+  }
+  if(arr2[0] == ':') {
+    result = arr1[0] / arr1[1];
+  }
+  return result;
+}
 
